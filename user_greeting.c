0f1afd8d56806663cf7aadb84c4f567241e9a21e
minka69btw <marks.sislovs@gmail.com>
2024-12-31 13:06:36 +0200

    simple user greeting program written in C

---
+++ user_greeting.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main() {
+    char name[50];
+
+    printf("Enter your name: ");
+    scanf("%49s", name);
+
+    printf("Hello, my name is  %s!\n",name);
+    printf("I am learning C!\n");
+    printf("C is awesome!\n");
+
+    return 0;
+}
+
+
