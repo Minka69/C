72a7deb8d7d666f84c21ae8b631c7e3d02f63c20
minka69btw <marks.sislovs@gmail.com>
2024-12-31 13:14:02 +0200

    Added age function

--- user_greeting.c
+++ user_greeting.c
@@ -2,15 +2,18 @@
 
 int main() {
     char name[50];
+    int age;
 
     printf("Enter your name: ");
     scanf("%49s", name);
 
-    printf("Hello, my name is  %s!\n",name);
+    printf("Enter your age: ");
+    scanf("%d", &age);
+
+    printf("Hello, my name is %s and I'm %d years old.\n", name, age);
     printf("I am learning C!\n");
     printf("C is awesome!\n");
 
     return 0;
 }
-
 
